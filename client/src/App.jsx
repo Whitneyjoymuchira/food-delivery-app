bacb06fbaac1976163417024f75a2fe26f2e6d3a
Whitneyjoymuchira <whitneymuchira@gmail.com>
2023-11-15 08:32:19 +0300

    changes: added css for app height and width

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
+// import { Routes,Route } from "react-router-dom";
 
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-red-700">HELLO WORLD</h1>
+      <h1 className="min-h-screen w-screen h-auto flex flex-col items-center justify-center">HELLO WORLD</h1>
+
+      
     </div>
   )
 }
 
-export default App
+export default App;
