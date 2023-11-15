42ebe5c91c4ab7725dd7fe475f3393eff60af849
Whitneyjoymuchira <whitneymuchira@gmail.com>
2023-11-15 14:01:21 +0300

    changes:created login bg image

---
+++ client/src/assets/index.js
@@ -0,0 +1,7 @@
+export {default as Avatar} from './img/avatar.png';
+export {default as Logo} from './img/logo.png';
+export { default as heroBg } from "./img/heroBg.png";
+// export { default as Mango } from "./img/logo.svg";
+export { default as Delivery } from "./img/delivery.png";
+// export { default as Bill } from "./img/logo.svg";
+export { default as logoBg } from "./img/package.jpg";
--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,9 +1,12 @@
-
+import { logoBg } from "../assets"
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className="w-screen h-screen overflow-hidden relative flex" >
+      <img 
+      className="w-full h-full object-cover top-0 left-0 absolute"
+      src={logoBg} 
+      alt="our iconic packaging" />
     </div>
   )
 }
