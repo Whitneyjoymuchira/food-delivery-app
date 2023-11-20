ad985d17230c83add81766a87051bcd2bc1021bd
Whitneyjoymuchira <whitneymuchira@gmail.com>
2023-11-20 14:45:32 +0300

    changes:
    changes: Added import statements for various icons
    
    Added import statements for various icons, including FaEnvelope, FaLock,
    FaCloudUploadAlt, and more.

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,28 @@
+import {FaEnvelope,FaLock,FaCloudUploadAlt,FaCheck,FaArrowLeft}from "react-icons/fa";
+import{FcGoogle,FcClearFilters}from 'react-icons/fc';
+import{MdShoppingCart,MdSearch,MdDelete,MdLogout}from "react-icons/md";
+import {BsFillBellFill,BsToggles2,BsExclamationTriangleFill}from "react-icons/bs";
+import {IoFastFood,IoBasket} from "react-icons/io";
+import {BiChevronsRight} from "react-icons/bi";
+import {HiCurrencyDollar} from "react-icons/hi2"
+
+export {
+    FaEnvelope,
+    FaLock,
+    FaCloudUploadAlt,
+    FaCheck,
+    FaArrowLeft,
+    FcGoogle,
+    FcClearFilters,
+    MdShoppingCart,
+    MdSearch,
+    MdDelete,
+    MdLogout,
+    BsFillBellFill,
+    BsToggles2,
+    BsExclamationTriangleFill,
+    IoFastFood,
+    IoBasket,
+    BiChevronsRight,
+    HiCurrencyDollar
+}
