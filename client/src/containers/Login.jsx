6b708c450aa9962d6f2ab76e6b0248623b1381c2
Whitneyjoymuchira <whitneymuchira@gmail.com>
2023-11-20 11:53:04 +0300

    changes:created logo element

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,14 +1,30 @@
-import { logoBg } from "../assets"
+import { logoBg, Logo } from "../assets";
 
 const Login = () => {
-  return (
-    <div className="w-screen h-screen overflow-hidden relative flex" >
-      <img 
-      className="w-full h-full object-cover top-0 left-0 absolute"
-      src={logoBg} 
-      alt="our iconic packaging" />
-    </div>
-  )
-}
+	return (
+		<div className="w-screen h-screen overflow-hidden relative flex">
+			{/* background image */}
+			<img
+				className="w-full h-full object-cover top-0 left-0 absolute font-thin"
+				src={logoBg}
+				alt="our iconic packaging"
+			/>
+			{/* content box */}
+			<div
+				className="flex flex-col h-full items-center w-[80%] md:w-[508px] lg:w-[408px] z-10
+			backdrop-blur-md p-4 px-4 py-12
+			bg-cardOverlay"
+			>
+				{/* logo */}
+				<div className="flex items-center justify-start gap-3 w-full">
+					<img src={Logo} alt="logo" className="w-8" />
+					<p className="text-textColor font-semibold text-2xl">
+						EatOut
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Login
+export default Login;
