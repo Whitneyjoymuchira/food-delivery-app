5e04d3c792f6038c18bfa029dc22b259753b3b5a
Whitneyjoymuchira <whitneymuchira@gmail.com>
2023-11-28 10:15:28 +0300

    Refactor main.jsx to include AnimatePresence in the render
    This change refactors the main.jsx file to include the AnimatePresence
    component from the framer-motion library in the render method. By doing
    so, it enables smoother and more visually appealing transitions in the
    application. This change improves the user experience and adds a touch
    of animation to the UI.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
+import{AnimatePresence} from "framer-motion"
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-  <App />
-    </Router>
-  
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Router>
+			<AnimatePresence>
+				<App />
+			</AnimatePresence>
+		</Router>
+	</React.StrictMode>
+);
